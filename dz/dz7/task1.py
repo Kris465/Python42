7bfd343386c3cfc910638c392b17769e0275bb20
Kris <oaisjdfoijsf@ijdfoasj>
2024-11-10 19:13:26 +0300

    task1 in dz7 is done

--- dz/dz7/task1.py
+++ dz/dz7/task1.py
@@ -2,7 +2,7 @@ num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
 for i in range(num1, num2 + 1):
     if i % 7 == 0:
-        print(i)
+        print(i, end='')
 
 print(*[i for i in range(num1, num2 + 1) if i % 7 == 0])
 [print(i) for i in range(num1, num2 + 1) if i % 7 == 0]
