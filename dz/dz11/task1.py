870de24b973ea66063615ce4827d2a9f8997ee77
Kris <oaisjdfoijsf@ijdfoasj>
2024-12-11 21:32:40 +0300

    task11

--- dz/dz11/task1.py
+++ dz/dz11/task1.py
@@ -1,7 +1,7 @@
 n = int(input("Введите сторону: "))
 
 # квадратик
-# print(*[''.join([" * " for elem in range(n)]) + "\n" for i in range(n)])
+print(*[''.join([" * " for elem in range(n)]) + "\n" for i in range(n)])
 
 # диагональка
 
@@ -12,14 +12,23 @@ for i in range(n):
         else:
             print("   ", end='')
     print()
-
 print()
 
 for i in range(n):
     for j in range(n):
-        if j <= i:
-            print("   ", end='')
-        else:
+        if j >= i:
             print(" * ", end='')
+        else:
+            print("   ", end='')
     print()
+print()
 
+# другая диагональка
+
+for i in range(n):
+    for j in range(n):
+        if j >= n - 1 - i:
+            print("*", end='')
+        else:
+            print(' ', end='')
+    print()
