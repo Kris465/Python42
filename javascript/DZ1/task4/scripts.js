81b5fe21bbced2a5f0dca7857e8aff0b03486b53
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-12 20:24:07 +0300

    Some changes

--- javascript/DZ1/task4/scripts.js
+++ javascript/DZ1/task4/scripts.js
@@ -3,7 +3,7 @@ document.getElementById('Button').addEventListener('click', function() {
     const message = document.getElementById('message')
 
     if (radius) {
-        message.textContent = `Площадь окружности: ${2 * 3.14 * radius}`
+        message.textContent = `Площадь окружности: ${2 * Math.PI * radius} квадратных см`
     }else{
         message.textContent = 'Пожалуйста введите радиус окружности'
     }
