4e1e51b52a8e809769e18814118a066548078e33
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-19 19:32:19 +0300

    complete

---
+++ javascript/js2/task2/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const password = prompt("Введите пароль", "")
+    "Step", "Web", "JavaScript"
+    console.log(password)
+    const password2 = prompt("Введите пароль", "")
+    null
+    console.log(password2)
+
+     
+
+    
+});
