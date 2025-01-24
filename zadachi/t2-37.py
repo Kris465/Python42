7c8deb78a2db0fe98005125aabd2ab0175795618
Kris <nataza7394@gmail.com>
2025-01-24 21:00:55 +0300

    t2-37 done

---
+++ zadachi/t2-37.py
@@ -0,0 +1,9 @@
+def find_pair_number(k):
+    if k < 1 or k > 180:
+        return " K вне диапазона"
+    index = k - 1
+    pair_number = index // 2 + 1
+    return pair_number
+k = int(input("введите число K: "))
+reault = find_pair_number(k)
+print(f"номер пары цифр для K = {k}: {reault}")
