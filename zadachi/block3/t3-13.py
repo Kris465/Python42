df3d9e9309b79a07761c7726a8341ec912c61f37
Kris <nataza7394@gmail.com>
2025-01-31 19:33:15 +0300

    t3-13.py

---
+++ zadachi/block3/t3-13.py
@@ -0,0 +1,29 @@
+# A)
+A = True
+B = False
+print(not (A and B))
+
+# B)
+A = False
+B = True
+print(not (A and B))
+
+# C)
+A = True
+B = False
+print(not A or B)
+
+# D)
+A = False
+B = True
+print(not A or B)
+
+# E)
+A = True
+B = False
+print(A or not B)
+
+# F)
+A = False
+B = True
+print(A or not B)
