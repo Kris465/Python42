2ba0dbd81cc5407bba5481c41f27a54b51b63e12
Kris <oaisjdfoijsf@ijdfoasj>
2025-02-02 19:47:04 +0300

    complete

---
+++ javascript/DZ2/task_10/script.js
@@ -0,0 +1,8 @@
+document.getElementById("yearButton").addEventListener("click", function() {
+    const year = document.getElementById("dateInput").value;
+    const message = document.getElementById("result")
+
+    good = new Date(year);
+    good.setDate(good.getDate() + 1);
+    message.textContent = good;
+});
