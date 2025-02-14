d0b5d955ebbf89efe8118ff371f4c16c395029e8
Kris465 <nataza7394@gmail.com>
2025-02-14 19:45:52 +0300

    :3

---
+++ zadachi/block3/t3-28.py
@@ -0,0 +1,30 @@
+A = 120
+B = 240
+C = -33
+
+# a)
+if A > 100 and B > 100:
+    print(True)
+
+# б)
+if A // 5 or B // 5:
+    print(True)
+    
+# в)
+if A > 0 or B > 0:
+    print(True)
+    
+# г)
+if A // 3 and B // 3 and C //3:
+    print(True)
+    
+# д)
+if A > 50 or B > 50 or C > 50:
+    print(True)
+    
+# е)
+if A < 0 or B < 0 or C < 0:
+    print(True)
+    
+else: 
+    print(False)
