dba4921899f574086a2f7b7ac64c0203d0735f72
Kris465 <nataza7394@gmail.com>
2025-02-14 19:34:07 +0300

    :3

---
+++ zadachi/block3/t3-27.py
@@ -0,0 +1,31 @@
+x = 10
+y = 6
+
+# a)
+if x > 2 and y > 3:
+    print(True)
+# б)
+if x > 1 or y > -2:
+    print(True)
+# в)
+if x >= 0 and y < 5:
+    print(True)
+# г)
+if x > 3 or x < -1:
+    print(True)
+# д)
+if x > 3 and x < 10:
+    print(True)
+# е)
+if not x > 2:
+    print(True)
+# ж)
+if not x > 0 and x < 5:
+    print(True)
+# з)
+if 10 < x <= 20:
+    print(True)
+# и)
+if 0 < y <= 4 and x < 5:
+    print(True)
+    
