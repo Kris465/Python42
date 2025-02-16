3cd2e30271f2168105be0bb801fdd48e3d653b13
Kris465 <nataza7394@gmail.com>
2025-02-16 17:48:51 +0300

    done

---
+++ zadachi/block3/t3-32.py
@@ -0,0 +1,71 @@
+#a)
+y = 1
+x = -2
+
+if y >= 1 and x <= -2:
+    print(True)
+else:
+    print(False)
+
+#b)
+y = 1.5
+x = 3
+
+if y >= -2 and y <= 1.5 and x >= 3 and x >= -3:
+    print(True)
+else:
+    print(False)
+
+#c)
+y = 1
+x = -2
+
+if y >= 1 and x <= -2:
+    print(True)
+else:
+    print(False)
+
+#d)
+y = 4
+x = 1
+
+if y >= 2 and x >= 1:
+    print(True)
+else:
+    print(False)
+
+#e)
+y = 2
+x = 2
+
+if (y < -1 or y >= 1) and x >= 1:
+    print(True)
+else:
+    print(False)
+
+#f)
+y = 1
+x = 2
+
+if (y >= 1 or y < -1.5) and x >= 2:
+    print(True)
+else:
+    print(False)
+
+#g)
+y = -1
+x = 3
+
+if y <= -1 and x != 0 and x >=1:
+    print(True)
+else:
+    print(False)
+
+#h)
+y = -1
+x = -2
+
+if (y <= 0.5 and y >= 1.5 or y >= -1) and x >= -2:
+    print(True)
+else:
+    print(False)
