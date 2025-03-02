78d00b9701e92a4682ac77bfc58b577de7b6b19f
Kris465 <nataza7394@gmail.com>
2025-03-02 20:15:20 +0300

    task15

--- zadachijs/block1/task15.js
+++ zadachijs/block1/task15.js
@@ -1,6 +1,32 @@
 // A
 
 const prompt = require('prompt-sync')();
-const a = prompt("Введите число x1: ")
+let x = prompt("Введите число x1: ")
 // const b = prompt("Введите число x2: ")
-console.log(`${(-1 / (a * a))}`);
+console.log(`${(-1 / (x1 * x1))}`);
+console.log(``)
+// Б
+
+// const prompt = require('prompt-sync')();
+let a = prompt("Введите число a: ")
+let b = prompt("Введите число b: ")
+let c = prompt("Введите число c: ")
+
+// const b = prompt("Введите число x2: ")
+console.log(`${(a / (b * c))}`);
+console.log(``)
+
+// В
+
+let a = prompt("Введите число a: ")
+let b = prompt("Введите число b: ")
+let c = prompt("Введите число c: ")
+
+console.log(`${((a / b) * c)}`);
+
+// Г
+
+let a = prompt("Введите число a: ")
+let b = prompt("Введите число b: ")
+
+console.log(`${((a + b) / 2)}`);
