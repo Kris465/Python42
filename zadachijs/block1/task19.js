58078511ff57a262e11226b5803db062d16f62c4
Kris465 <nataza7394@gmail.com>
2025-03-07 19:58:13 +0300

    task19

---
+++ zadachijs/block1/task19.js
@@ -0,0 +1,30 @@
+a)
+
+x = 10
+x = -10
+console.log(`${x}`)
+
+b) 
+
+x = 17.5
+x =  -2 * x
+console.log(`${x}`)
+
+c)
+
+x = 60
+x = x - 1
+x = 0
+console.log(`${x}`)
+
+d)
+
+x = -50
+x = -25
+k = 1
+x = x + k
+console.log(`${x}`)
+
+
+
+ 
