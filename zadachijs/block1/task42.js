963c734d12a7e7b56b64dabe78ac9b5820274c7a
Kris465 <nataza7394@gmail.com>
2025-03-16 19:07:21 +0300

    task41

---
+++ zadachijs/block1/task42.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+const g = prompt('Введите число "g": ');
+const e = prompt('Введите число "e": ');
+const f = prompt('Введите число "f": ');
+const h = prompt('Введите число "h": ');
+console.log((e + f / 2) / 3)
+console.log(h ** 2 - g)
+console.log(Math.sqrt((g - h) ** 2 - 3 * sin(e))
---
+++ zadachijs/block1/task43.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+const a = prompt('Введите число "a": ');
+const b = prompt('Введите число "b": ');
+console.log((a + b) / 2)
+console.log(sqrt((a * b)))
