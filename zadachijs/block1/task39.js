f135e5a02d27d988727233ea2ef0954ad3311519
Kris465 <nataza7394@gmail.com>
2025-03-16 20:33:33 +0300

    task39

--- zadachijs/block1/task39.js
+++ zadachijs/block1/task39.js
@@ -1,6 +1,12 @@
-let x = Number(prompt('Введите число для первой переменной: '))
-let y = Number(prompt('Введите число для второй переменной: '))
+// let x = Number(prompt('Введите число для первой переменной: '))
+// let y = Number(prompt('Введите число для второй переменной: '))
+
+let x = 10
+let y = 20
 let dividend = (x + ((2 + y) / (x * x)))
 let divider = y + (1 / Math.sqrt(x * x + 10))
-let result_1 = alert(`Ответ: ${(dividend / divider)}`)
-let result_2 = 2.8*Math.sin(x) + 
+let result_1 = (dividend / divider)
+let result_2 = 2.8*Math.sin(x) + Math.abs(x)
+
+console.log(result_1)
+console.log(result_2)
