320c02454d1e1f2594ccea37b0c4b319ee1baf00
Kris <oaisjdfoijsf@ijdfoasj>
2025-03-21 21:49:57 +0300

    oop task

---
+++ oop/dz1/task3.py
@@ -0,0 +1,20 @@
+class Stadion:
+    def __init__(self, title, country, city):
+        self.title = title
+        self.date = input("Введите дату: ")
+        self.country = country
+        self.city = city
+        self.__countity = ""
+        
+        def set_countity(self):
+            countity = input("Введите страну: ")
+            self.__countity = countity
+        
+        def get_countity(self):
+            return self.__countity
+                     
+        stadion = Stadion("Roma", "USA", "Hollywood")
+        print(stadion.title, stadion.city, stadion.country)
+        
+
+
