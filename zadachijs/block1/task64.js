b6ea22cc7063227d1a7be007883dd712eee43e36
Kris465 <nataza7394@gmail.com>
2025-03-30 19:19:24 +0300

    задача 64 готова

---
+++ zadachijs/block1/task64.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+const number = prompt("Введите 6-ти значное число: ");
+
+let n1 = parseInt(number[0]) 
+let n2 = parseInt(number[1])
+let n3 = parseInt(number[2])
+let n4 = parseInt(number[3])
+let n5 = parseInt(number[4])
+let n6 = parseInt(number[5])
+
+if ( n1 + n2 + n3 == n4 + n5 + n6) {
+    console.log("Поздравляем! Ваше число счастливое!")
+} else {
+    console.log("К сожалению ваше число не счастливое. Но ничего в следующий раз повезёт!")
+}
