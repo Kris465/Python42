4c4ce86fd670e142e252474f0a494b4ccc3349cb
Kris465 <nataza7394@gmail.com>
2025-04-06 18:59:04 +0300

    task71

---
+++ zadachijs/block1/task69.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+// first
+
+for (number_conor_first in range(1, 5)) {
+    alert(`Введите координады ${number_conor} угла`)
+    let x = Number(prompt('Введите координату x: '))
+    let y = Number(prompt('Введите координату y: '))
+}
+// second
+
+for (number_conor_second in range(1, 5)) {
+    alert(`Введите координады ${number_conor} угла`)
+    let x = Number(prompt('Введите координату x: '))
+    let y = Number(prompt('Введите координату y: '))
+}
