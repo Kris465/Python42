e3084dc96e6902de5c19c485696e3db5da04e0f3
Kris465 <nataza7394@gmail.com>
2025-04-06 19:23:29 +0300

    task70

---
+++ zadachijs/task72.js
@@ -0,0 +1,10 @@
+
+let a = 5.5;
+let b = 3.2;
+let c = 7.8;
+
+let largest = Math.max(a, b, c);
+console.log("Наибольшее число:", largest);
+
+let smallest = Math.min(a, b, c);
+console.log("Наименьшее число:", smallest);
