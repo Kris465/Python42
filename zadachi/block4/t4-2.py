92422f1aed52e2c0e9d783df574df999f944f189
Kris465 <nataza7394@gmail.com>
2025-04-18 21:07:37 +0300

    task62

---
+++ zadachi/block4/t4-2.py
@@ -0,0 +1,10 @@
+from math import sin, radians
+
+x = float(input('Введите значение "x": '))
+
+if x > 0:
+    y = (sin(radians(x**2)))
+    print(f'При "x" = {x}, "y" = {round(y, 2)}')
+else:
+    y = 1 + 2*(sin(radians(x)**2))
+    print(f'При "x" = {x}, "y" = {round(y, 2)}')
