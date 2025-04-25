abdf99f9c00bc212b31c15c454a1774718202348
Kris465 <nataza7394@gmail.com>
2025-04-25 19:23:17 +0300

    py

---
+++ zadachi/block4/t4-6.py
@@ -0,0 +1,6 @@
+number1 = int(input("Введите первое число: "))
+number2 = int(input("Введите второе число: "))
+if number1 > number2:
+    print("Первое больше второго!")
+else:
+    print("Второе больше первого!")
