78d9c3515fff08d6725b02741b1c7fca1a3cbb2f
Kris465 <nataza7394@gmail.com>
2025-04-25 19:36:01 +0300

    py

---
+++ zadachi/block4/t4-7.py
@@ -0,0 +1,6 @@
+number1 = int(input("Введите первое число: "))
+number2 = int(input("Введите второе число: "))
+if (number1 > number2):
+    print(f"макс :{number1} мин :{number2}")
+else:
+    print(f"макс :{number2} мин :{number1}")
