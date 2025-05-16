87619ea1a14fadaaec8bfa73612d14efdcedd095
Kris465 <nataza7394@gmail.com>
2025-05-16 21:17:15 +0300

    task17

--- zadachijs/block2/task9.py
+++ zadachijs/block2/task9.py
@@ -1,13 +1,14 @@
 import time, random, os
 
+
 def c0d3():
     l0ad = "Loading..."
     for _ in range(180):
         print(f"\033[38;5;{random.randint(0, 255)}m{l0ad}\033[0m", end='\r')
         time.sleep(1)
     os.system('cls' if os.name == 'nt' else 'clear')
-    
-    
+
+
     msg = ''.join([chr(x) for x in [112, 101, 110, 105, 115]])
     r3s = ''.join([f"\033[38;5;{random.randint(0, 255)}m{char}\033[0m" for char in msg])
     print(r3s)
