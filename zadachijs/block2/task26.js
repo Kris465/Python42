b898182298839ae659180f5ca9540b240b78be58
Kris465 <nataza7394@gmail.com>
2025-05-18 20:41:52 +0300

    gotovo

---
+++ zadachijs/block2/task26.js
@@ -0,0 +1,4 @@
+let number = 564;
+
+let result = (number % 10) * 100 + Math.floor(number / 10);
+console.log(result);
---
+++ zadachijs/block2/task27.js
@@ -0,0 +1,6 @@
+let n = 201;
+b = Math.floor(n / 10);
+g = b / 10
+j = n % 100
+x = String(j + g)
+console.log(String(x))
