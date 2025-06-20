f679894205a87c7099199360722ada62e40fbde5
Kris465 <nataza7394@gmail.com>
2025-06-20 21:07:46 +0300

    task45

---
+++ zadachi/block4/task45.py
@@ -0,0 +1,10 @@
+a = int(input("Введите цифру: "))
+b = int(input("Введите цифру: "))
+c = int(input("Введите цифру: "))
+
+if a == b or a == c:
+    print("Числа равны")
+if b == a or b == c:
+    print("Числа равны")
+else:
+    print("Числа не равны")
