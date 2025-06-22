7c5386ce36731666e2b39ffae180e73066a1995e
Kris465 <nataza7394@gmail.com>
2025-06-22 20:57:37 +0300

    task58

---
+++ zadachi/block4/task58.py
@@ -0,0 +1,13 @@
+a = input('Введите трёхзначное число: ')
+
+# a
+if '4' in a or '7' in a:
+    print('да')
+else:
+    print('нет')
+
+#  б
+if '3' in a or '6' in a or '7' in a:
+    print('да')
+else:
+    print('нет')
