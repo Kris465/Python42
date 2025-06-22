9ba1fa153894a0672f0700bf3f20d33475e29e1e
Kris465 <nataza7394@gmail.com>
2025-06-22 20:44:01 +0300

    task56

---
+++ zadachi/block4/task57.py
@@ -0,0 +1,8 @@
+x = int(input("Введите трехзначное число: "))
+n = int(input("Введите число: "))
+if '6' in str(x):
+    print(f"В вашем числе {x}, содержится число 6)")
+elif str(n) in str(x):
+    print('есть n')
+else:
+    print("ну сорр")
