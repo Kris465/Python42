1ea3b0758997975571cffa1b419fc8e85f74a9e7
Kris465 <nataza7394@gmail.com>
2025-06-22 18:16:04 +0300

    task49

--- zadachi/block4/task49.py
+++ zadachi/block4/task49.py
@@ -1,3 +1,5 @@
+import math
+
 a = int(input("Введите число a: "))
 b = int(input("Введите число b: "))
 c = int(input("Введите число c: "))
@@ -8,9 +10,12 @@ else:
     D = b ** 2 - 4 * a * c
 
     if D < 0:
-        print(f"Ну дискриминант меньше нуля - {D}")
+        print(f"Ну дискриминант меньше нуля = {D}")
     elif D == 0:
         x = -b // (2 * a)
-        print(f"Корнь - {x}")
+        print(f"Корнь = {x}")
     else:
-        
+        sqrt_D = math.sqrt(D)
+        x1 = (-b + sqrt_D) // (2 * a)
+        x2 = (-b - sqrt_D) // (2 * a)
+        print(f"Корень первый = {x1}, второй корень = {x2}")
