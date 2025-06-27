64f0c0d25649b278bdca276ba8b94d49d0694688
Kris465 <nataza7394@gmail.com>
2025-06-27 19:17:46 +0300

    60

---
+++ zadachi/block4/task59.py
@@ -0,0 +1,15 @@
+a = input('Введите четырёх значное число число: ')
+b = input('Введите цифру: ')
+
+if 999 < a < 10000 and 0 < b < 10:
+    print('ответ на А')
+    if '4' in a:
+        print('да')
+    else:
+        print('нет')
+
+    print('ответ на B')
+    if 'b' in a:
+        print('да')
+    else:
+        print('нет')
---
+++ zadachi/block4/task60.py
@@ -0,0 +1,13 @@
+a = input('Введите четырехрёхзначное число: ')
+
+# a
+if '2' in a or '7' in a:
+    print('да')
+else:
+    print('нет')
+
+#  б
+if '3' in a or '6' in a or '7' in a:
+    print('да')
+else:
+    print('нет')
