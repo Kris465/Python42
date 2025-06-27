7bbf3eebd2319a337840f639d7ff552a878e0741
Kris465 <nataza7394@gmail.com>
2025-06-27 21:47:51 +0300

    tasks

--- zadachi/block4/task64.py
+++ zadachi/block4/task64.py
@@ -1,9 +1,11 @@
 def is_lucky_ticket(number):
     num_str = str(number)
-    
+
+
     if len(num_str) !=6:
         return False
- 
+
+
     left_part = num_str[:3]
     right_part = num_str[3:]
 
@@ -11,6 +13,7 @@ def is_lucky_ticket(number):
     sum_right = sum(int(digit) for digit in right_part)
 
     return sum_left == sum_right
+
 
 ticket = 123420
 print(is_lucky_ticket(ticket))
