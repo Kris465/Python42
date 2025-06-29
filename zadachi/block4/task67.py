5ba824710d3064ee07b3fbe6ee1b94b5151529ae
Kris465 <nataza7394@gmail.com>
2025-06-29 18:32:50 +0300

    tasks

---
+++ zadachi/block4/task67.py
@@ -0,0 +1,17 @@
+
+k = int(input("k = "))
+
+if k % 1:
+    print("пнд")
+elif k % 2:
+    print("втр")
+elif k % 3:
+    print("срд")
+elif k % 4:
+    print("чтв")
+elif k % 5:
+    print("птн")
+elif k % 6:
+    print("сбт")
+elif k % 7:
+    print("вск")
