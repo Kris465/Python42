c39a49ef4b23961ff37332435e2b18b4b9758110
Kris465 <nataza7394@gmail.com>
2025-07-11 21:20:20 +0300

    task 92

--- zadachi/block4/task92.py
+++ zadachi/block4/task92.py
@@ -1,7 +1,7 @@
 x = int(input("Введите x: "))
-if x < 0 or x > 0:
+if x < 0 or x > 5:
     y = 1
     print(f"Результат Y такой: {y}")
-if x == 0:
+if x == 1:
     y = 5
     print(f"Результат Y такой: {y}")
