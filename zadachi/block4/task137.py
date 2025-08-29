2efd9c9607743650b229f3e5bacc02217dbb7ff9
Kris465 <nataza7394@gmail.com>
2025-08-29 19:04:53 +0300

    tasks

--- zadachi/block4/task137.py
+++ zadachi/block4/task137.py
@@ -1,11 +1,12 @@
 
 k = int(input("введите число: "))
-if k <= 1:
-    print(k)
-elif k <= 180:
-    n = k - 1
-    num = 10 + (n - 1) // 2
-    print(num // 10 if n % 2 == 1 else num % 10)
-else:
-    print("не в ходит диапазон чисел")
-
+i = 10
+spisok = []
+while i < 100:
+    a = i // 10
+    b = i % 10
+    spisok.append(a)
+    spisok.append(b)
+    i += 1
+print(spisok)
+print(spisok[k])
