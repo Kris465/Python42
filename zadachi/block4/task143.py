493cfc8dddbbb982ae341bb712f6c231a04c88a0
Kris465 <nataza7394@gmail.com>
2025-08-31 18:08:19 +0300

    tasks 4 done

--- zadachi/block4/task143.py
+++
@@ -1,5 +0,0 @@
-n = int(input('Введите число: '))
-a = int(input('Введите колличество повторений: '))
-
-for i in range(a):
-    print(n, end=" ")
---
+++ zadachi/block5/task4.py
@@ -0,0 +1,9 @@
+# часть а)
+print("часть а):")
+for i in range(10, 26):
+    print(f"{i} {i + 0.4}")
+
+print("\nчасть б):")
+# часть б)
+for i in range(25, 36):
+    print(f"{i} {i + 0.5} {i - 1.2}")
