807a3403080c274f03a9c1c06bd5ad8fed34d57a
Kris465 <nataza7394@gmail.com>
2025-10-26 18:37:56 +0300

    tasks

---
+++ zadachi/block5/task52.py
@@ -0,0 +1,8 @@
+marks = input('Введите оценки всех учеников через пробел: ').split(' ')
+sum = 0
+
+for i in marks:
+    sum += int(i)
+sr = round(sum / len(marks), 2)
+
+print(f'Средняя оценка: {sr}')
